58147e6a8ccfba3fc416cb870193f234bfb882a6
Pedro Rotta <43259583+pedrorotta@users.noreply.github.com>
2022-02-15 12:08:56 -0500

    Add files via upload

---
+++ clase 2/srcmañana15-2/script.js
@@ -0,0 +1,18 @@
+//cuando yo haga click en el botón 
+//aparezca la imágen. 
+
+
+
+//Local a la función mostrarimagen()
+
+function mostrarimagen(){
+ let divImagen = document.getElementById("imagen");
+ divImagen.style.display = "block";
+}
+
+function ocultarimagen(){
+ let divImagen = document.getElementById("imagen");
+ divImagen.style.display = "none";
+}
+
+//como se podría hacer para que cuando la imagen aparece el botón cambia a ocultar imagen, y cuando se oculta el botón vuelve a cambiar a mostrar imagen
