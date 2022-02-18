ceebb28fbc0027f835fd265dffd52d96ff26ddb5
Pedro Rotta <43259583+pedrorotta@users.noreply.github.com>
2022-02-18 16:10:18 -0500

    Add files via upload

---
+++ clase5/src2/script.js
@@ -0,0 +1,11 @@
+let openmodal = ()=>{
+ SectionModal = document.getElementById("SectionModal");
+ SectionModal.className = "modalview";
+}
+
+
+let closemodals = ()=>{
+ SectionModal = document.getElementById("SectionModal");
+ SectionModal.className = "modalclose";
+
+}
